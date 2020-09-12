de02976b34b97d0985e3690c6a5079c8712e65ec
Aki <Akikami77@gmail.com>
2020-09-12 04:14:09 +0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# howtostart
+# howtostart (Programlamaya Giriş)
+    Bu dökümanlar ingilizce kaynakların ve kendi deneyimlerimiz sonucunda
+    ortaya çıkmıştır.  [Dökümanı Görüntüle](kodbilen.com)
+
+# Author
+- Aki
+
+# Links
+- [Programlama Nedir](Example.com/what)
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+       <router-link to="/About">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+ //https://www.youtube.com/watch?v=gQv3uUe3bA8&ab_channel=AdemIlter
+
+export default {
+  name: "App",
+
+  components: {
+  
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
